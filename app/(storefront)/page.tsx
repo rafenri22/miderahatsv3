d88d5e12bd52e2e401460982a88308daa3a7534c
Rafky <rafky.gamers@gmail.com>
2024-12-10 18:20:23 +0700

    first commit

---
+++ app/(storefront)/page.tsx
@@ -0,0 +1,13 @@
+import { CategoriesSelection } from "../components/storefront/CategorySelection";
+import { FeaturedProducts } from "../components/storefront/FeaturedProducts";
+import { Hero } from "../components/storefront/Hero";
+import { Navbar } from "../components/storefront/Navbar";
+
+export default function IndexPage() {
+  return (
+    <div>
+      <Hero />
+      <FeaturedProducts />
+    </div>
+  );
+}
